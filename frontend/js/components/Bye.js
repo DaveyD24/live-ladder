ad16f1764446b2e8ed7b124ce75d10d21745ed17
Davey Dyer <69884791+DaveyD24@users.noreply.github.com>
2026-05-09 15:17:40 +1000

    fix(byes) Mobile responsive

--- frontend/js/components/Bye.js
+++ frontend/js/components/Bye.js
@@ -5,9 +5,13 @@ function render(byes) {
         imgString += `<img class='w-10' src='${imagePath}'/>`
     });
     return `
-        <div class="w-9/10 lg:w-95/100 desktop:w-9/10 max-w-200 lg:max-w-full desktop:max-w-200 relative bg-white dark:bg-neutral-950 flex flex-row justify-center items-center gap-8 py-4 rounded-lg border border-slate-200 dark:border-neutral-700 col-span-2">
-            <p class="absolute left-1/30 text-md font-inter text-gray-700 dark:text-zinc-50">Byes</p>
-            ${imgString}
+        <div class="w-9/10 lg:w-95/100 desktop:w-9/10 max-w-200 lg:max-w-full desktop:max-w-200 relative bg-white dark:bg-neutral-950 flex flex-col gap-4 md:flex-row justify-center items-center py-4 rounded-lg border border-slate-200 dark:border-neutral-700 col-span-2
+            ">
+            <p class="md:absolute left-1/30 text-md font-inter text-gray-700 dark:text-zinc-50">Byes</p>
+            <div class='flex flex-row gap-8 overflow-x-auto md:overflow-x-visible no-scrollbar'>
+                ${imgString}           
+            </div>
+
         </div>
     `;
 }
