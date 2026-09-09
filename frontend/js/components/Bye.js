40b8ce381dddc6e9199e08f5c13a17e5d94038d5
Davey Dyer <69884791+DaveyD24@users.noreply.github.com>
2026-09-10 07:46:49 +1000

    fix(historical) increase bye container height for no byes

--- frontend/js/components/Bye.js
+++ frontend/js/components/Bye.js
@@ -5,7 +5,7 @@ function render(byes) {
         imgString += `<img class='w-10' src='${imagePath}'/>`
     });
     return `
-        <div class="w-9/10 lg:w-95/100 desktop:w-9/10 max-w-200 lg:max-w-full desktop:max-w-200 relative bg-white dark:bg-neutral-950 flex flex-col gap-4 md:flex-row justify-center items-center py-4 rounded-lg border border-slate-200 dark:border-neutral-700 col-span-2
+        <div class="w-9/10 lg:w-95/100 desktop:w-9/10 max-w-200 lg:max-w-full desktop:max-w-200 min-h-16 relative bg-white dark:bg-neutral-950 flex flex-col gap-4 md:flex-row justify-center items-center py-4 rounded-lg border border-slate-200 dark:border-neutral-700 col-span-2
             transition-all duration-200 ease-in-out">
             <p class="md:absolute left-1/30 text-md font-inter text-gray-700 dark:text-zinc-50">Byes</p>
             <div class='flex flex-row gap-8 overflow-x-auto md:overflow-x-visible no-scrollbar'>
